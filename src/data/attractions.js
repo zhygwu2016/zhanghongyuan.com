8f94904aabb95c40bd36ddc3b80d4ab3a5b392be
Hongyuan Zhang <zhygwu2016@gmail.com>
2019-02-27 01:04:29 -0800

    First commit

---
+++ src/data/attractions.js
@@ -0,0 +1,67 @@
+const attractions = [
+  {
+    title: 'Enjoy the City',
+    description:
+      'Sept 22, 2016 in K St NW & 17th St NW, Washington D.C., RICOH GR II. I was wandering around the White House with my GR II after studying in the library. As darkness fell, lights were lit on the street.',
+    image: 'image_1.jpg',
+    className: 'w-50-l'
+  },
+  {
+    title: 'Jianxiang Bridge at Night',
+    description:
+      "Jianxiang Bridge is a major overpass on Beijing's 4th Ring Road. July 2, 2013 in Beijing, China.",
+    image: 'image_2.jpg',
+    className: 'w-third-l ml5-l'
+  },
+  {
+    title: 'Northland',
+    description:
+      "Jan 22, 2014 in Nong'an, Jilin, China.",
+    image: 'image_3.jpg',
+    className: 'w-third-l ml-auto-ns'
+  },
+  {
+    title: 'In the End',
+    description:
+      'Aug 29, 2013 in The Central Radio and Television Tower, Beijing, China.',
+    image: 'image_4.jpg',
+    className: 'w-40-l ml5-l mr5-l'
+  },
+  {
+    title: 'Yellow',
+    description:
+      'Sept 30, 2018 in The Seattle Public Library, Seattle.',
+    image: 'image_5.jpg',
+    className: 'w-30-l'
+  },
+  {
+    title: 'One night in Macau.',
+    description:
+      'One night in Macau,may love last forever. Jan 6, 2015 in Macau, China.',
+    image: 'image_6.jpg',
+    className: 'w-third-l ml5-l mr-auto-l'
+  },
+  {
+    title: 'Sunset in Yellow Mountain',
+    description:
+      'Jan 15, 2015 in Anhui, China.',
+    image: 'image_7.jpg',
+    className: 'w-third-l ml-auto-l mr5-l'
+  },
+  {
+    title: '1825 EYE STREET.',
+    description:
+      '1825 Eye Street at Washing DC, where I found an international foodcourt at that day. Sept 16, 2016 by iPhone, SKRWT & VSCO.',
+    image: 'image_8.jpg', 
+    className: 'w-third-l mr-auto-l'
+  },
+  {
+    title: 'Conrad Beijing',
+    description:
+      'Aug 21, 2015 in Beijing, China.',
+    image: 'image_9.jpg',
+    className: 'w-third-l ml-auto-l'
+  }
+];
+
+export default attractions;
---
+++ src/data/menu.js
@@ -0,0 +1,30 @@
+const menu = [
+  {
+    children: 'Hongyuan Zhang',
+    logo: true,
+    className: 'order-3-ns w-100 w-30-ns mb3 mb0-ns',
+    href: 'index.html'
+  },
+  {
+    children: 'About',
+    className: 'order-1-ns w-20',
+    href: 'about.html'
+  },
+  {
+    children: 'Resume',
+    className: 'order-2-ns w-20',
+    href: 'resume.html'
+  },
+  {
+    children: 'Publication',
+    className: 'order-4-ns w-20',
+    href: 'publication.html'
+  },
+  {
+    children: 'Contact',
+    className: 'order-5-ns w-20',
+    href: 'contact.html'
+  }
+];
+
+export default menu;
